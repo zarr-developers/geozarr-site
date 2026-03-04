f49094d9004ae9cb8b6388d8f8c036174267a605
Max Jones <14077947+maxrjones@users.noreply.github.com>
2026-03-03 21:23:22 -0500

    Add more resource links

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -42,6 +42,10 @@ export default defineConfig({
             link: "https://github.com/zarr-developers/geozarr-toolkit/",
           },
           {
+            text: "GeoZarr online validator",
+            link: "https://inspect.geozarr.org",
+          },
+          {
             text: "geozarr-examples",
             link: "https://developmentseed.org/geozarr-examples/",
           },
@@ -52,6 +56,10 @@ export default defineConfig({
           {
             text: "OGC SWG Charter",
             link: "https://github.com/zarr-developers/geozarr-spec/blob/main/CHARTER.adoc",
+          },
+          {
+            text: "SWG Meeting Minutes",
+            link: "https://hackmd.io/team/geozarr/folders/bMJiJHjvbga0Ce5YaR6oq?nav=overview",
           },
         ],
       },
